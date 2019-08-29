90a099c2286c6655dcb0ad2e554cc6a4d43b53f4
Utkarsh Singh <utkarsh97s@gmail.com>
2019-08-29 11:01:36 +0530

    Exception handling

---
+++ Exception.java
@@ -0,0 +1,17 @@
+package labexercise;
+
+import side.InvalidAgeException;
+
+public class Exception {
+	public static void main(String args[]) {
+		try {
+			CustomException exception1 = new CustomException();
+			exception1.setAge(25);
+			System.out.println(exception1.getAge());
+		}catch(InvalidAgeException m) {
+			System.out.println("Exception ocurred:"+ m);
+		}
+		
+	}
+
+}
